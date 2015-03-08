83fdd3f8919ecf0713a24d1fcb87a82732cf443d
Zhili Yang <zhya1033@colorado.edu>
2015-03-07 23:37:43 -0700

    completed 50/50

---
+++ contents/index.md
@@ -0,0 +1,4 @@
+---
+title: Instagram API
+template: index.jade
+---
---
+++ contents/js/app.js
@@ -0,0 +1,7 @@
+$( "#users" ).on( "click", function( event ) {
+    usersList()
+})
+
+$( "#medias" ).on( "click", function( event ) {
+    mediasList()
+})
