6285dc57a8a8610873f896d6407eda19b208633b
Zhili Yang <zhya1033@colorado.edu>
2015-03-07 23:37:43 -0700

    completed 50/50

---
+++ js/my/repos.js
@@ -0,0 +1,20 @@
+function myRepos(){
+
+    // display my repos in #list
+    // e.g., https://api.github.com/users/doubleshow/repos
+
+    $.get("https://api.github.com/users/zhya215/repos", github, function(data) {
+    	var myRepos = data
+
+    	$.get("/git-jquery/templates/myRepoList.jade", function(template){
+    		// render the template
+            var html = jade.render(template, {items: myRepos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            repoView(myRepos[0].full_name)
+    	})
+    })
+}
